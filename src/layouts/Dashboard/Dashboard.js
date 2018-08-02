ebff2bfc0401d4e41c8cb1eed9cd98c0ba181af2
Tobiah Rex <tobiah.rex@gmail.com>
2018-08-01 20:26:45 -0700

    added "Redirect" component to Dashboard (index route).

--- src/layouts/Dashboard/Dashboard.js
+++ src/layouts/Dashboard/Dashboard.js
@@ -1,40 +1,45 @@
 import React from 'react';
-import { Switch, Route, NavLink } from 'react-router-dom';
+import { Switch, Redirect, Route, NavLink } from 'react-router-dom';
 import dashboardRoutes from '../../routes/Dashboard';
 
 class Dashboard extends React.Component {
   render() {
     const activeStyle = { color: 'blue' };
     return (
-      <React.Fragment>
-        <h1>React-Boilerplate</h1>
-        <div>
-          <NavLink exact to="/" activeStyle={activeStyle}>
-            Home
-          </NavLink>
-          {' | '}
-          <NavLink to="/cryptos" activeStyle={activeStyle}>
-            Demo Crypto Table
-          </NavLink>
-          {' | '}
-          <NavLink to="/things" activeStyle={activeStyle}>
-            Demo CRUD
-          </NavLink>
-          {' | '}
-        </div>
-        <Switch>
-          {dashboardRoutes.map((prop, key) => {
-            return (
-              <Route
-                path={prop.path}
-                name={prop.name}
-                key={key}
-                component={prop.component}
-              />
-            );
-          })}
-        </Switch>
-      </React.Fragment>
+      <div className="content">
+        <React.Fragment>
+          <h1>React-Boilerplate</h1>
+          <div>
+            <NavLink exact to="/" activeStyle={activeStyle}>
+              Home
+            </NavLink>
+            {' | '}
+            <NavLink to="/cryptos" activeStyle={activeStyle}>
+              Demo Crypto Table
+            </NavLink>
+            {' | '}
+            <NavLink to="/things" activeStyle={activeStyle}>
+              Demo CRUD
+            </NavLink>
+            {' | '}
+          </div>
+          <Switch>
+            {dashboardRoutes.map((prop, key) => {
+              if (prop.redirect) {
+                return <Redirect from={prop.path} to={prop.to} key={key} />;
+              }
+              return (
+                <Route
+                  path={prop.path}
+                  name={prop.name}
+                  key={key}
+                  component={prop.component}
+                />
+              );
+            })}
+          </Switch>
+        </React.Fragment>
+      </div>
     );
   }
 }
