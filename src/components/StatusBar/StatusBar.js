11a1d02d976319037411a7f29cd144d2585ea316
Tobiah Rex <tobiah.rex@gmail.com>
2018-08-02 20:35:37 -0700

    updated and finished building the update method for StatusBar.

--- src/components/StatusBar/StatusBar.js
+++ src/components/StatusBar/StatusBar.js
@@ -16,10 +16,62 @@ class CrudTable extends React.Component {
     super(props);
 
     this.state = {
-      fetching: false,
-      error: false,
+      show: false,
+      message: '',
+      statusInfo: null,
       count: false,
     }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { status: stateStatus } = this.state;
+    const { status: nextStatus } = this.nextProps;
+
+    if ( // If fetching was successfully completed
+      stateStatus.fetching &&
+      !stateStatus.error &&
+      !nextStatus.error &&
+      !nextStatus.fetching
+    ) {
+      this.setState({
+        message: 'Database updated SUCCESSFULLY!',
+        status: {
+          error: nextStatus.error,
+          fetching: nextStatus.fetching,
+        },
+        show: true,
+      });
+      return true;
+
+    } else if ( // If we just started fetching
+      !nextStatus.error &&
+      nextStatus.fetching
+    ) {
+      this.setState({
+        message: 'API Request in Progress',
+        status: {
+          error: nextStatus.error,
+          fetching: nextStatus.fetching,
+        },
+        show: true
+      });
+      return true;
+    } else if ( // if fetching yielded an error
+      !stateStatus.error &&
+      nextStatus.error
+    ) {
+      this.setState({
+        message: 'Database update FAILED!',
+        status: {
+          error: nextStatus.error,
+          fetching: nextStatus.fetching,
+        },
+        show: true
+      });
+      return true;
+    }
+    ``// if there is some other reason this lifecycle method is called, then continue w/Reconcilliation.
+    return true;
   }
 
   determineType = () => {
