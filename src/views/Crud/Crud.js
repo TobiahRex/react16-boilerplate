12cb76f614536e0573223687191e7275e3e505fa
Tobiah Rex <tobiah.rex@gmail.com>
2018-08-02 17:08:51 -0700

    building out a from for entering a new input.

--- src/views/Crud/Crud.js
+++ src/views/Crud/Crud.js
@@ -19,8 +19,38 @@ class UserProfile extends Component {
   render() {
     return (
       <div className="content">
-        <Grid fluid />
-        <img src={avatar} alt="" />
+        <Grid fluid>
+          <Row>
+            <Col md={12}>
+              <Card
+                title="CRUD"
+                content={
+                  <form>
+                    <FormInputs
+                      ncols={['col-md-8', 'col-md-4']}
+                      properties={[
+                        {
+                          label: 'New Thing',
+                          type: 'Enter some data...',
+                          bsClass: 'form-control',
+                          placeholder: 'Username',
+                          defaultValue: 'michael23'
+                        },
+                        {
+                          label: 'Email address',
+                          type: 'email',
+                          bsClass: 'form-control',
+                          placeholder: 'Email'
+                        }
+                      ]}
+                    />
+                    <img src={avatar} alt="" />
+                  </form>
+                }
+              />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
